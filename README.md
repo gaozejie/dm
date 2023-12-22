cd351bd5a6a12d05d5b26819cb7eecd2b202d825
gaozejie <gaozejie>
2023-12-22 11:03:01 +0800

    v1.0.1

---
+++ README.md
@@ -0,0 +1,10 @@
+### dm-go-driver
+
+达梦数据库官方`go`驱动，版本号：8.1.3.12
+
+
+
+```
+go get github.com/gaozejie/dm
+```
+
---
+++ i18n/zm.go
@@ -0,0 +1,23 @@
+package i18n
+
+const Messages_zh_TW = `{
+  "language": "zh-Hant",
+  "messages": [
+    {
+      "id": "error.dsn.invalidSchema",
+      "translation": "DSN串必須以dm://開頭"
+    },
+    {
+      "id": "error.communicationError",
+      "translation": "網絡通信異常"
+    },
+    {
+      "id": "error.msgCheckError",
+      "translation": "消息校驗異常"
+    },
+    {
+      "id": "error.unkownNetWork",
+      "translation": "未知的網絡"
+    }
+  ]
+}`
